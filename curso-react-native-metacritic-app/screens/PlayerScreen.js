6f5481c1512907230ed907dc52f4173276f59c47
Jose Antonio Sánchez López <80800807+vikiii22@users.noreply.github.com>
2024-10-27 17:16:05 +0100

    player on error

--- curso-react-native-metacritic-app/screens/PlayerScreen.js
+++ curso-react-native-metacritic-app/screens/PlayerScreen.js
@@ -1,28 +1,41 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { View, Text, StyleSheet, Dimensions } from 'react-native';
 import { Video } from 'expo-av';
 
 const PlayerScreen = ({ route }) => {
   const { channelName, channelUrl } = route.params;
-  console.log('channelName:', channelName);
-  console.log('channelUrl:', channelUrl);
+  const videoRef = useRef(null); // Ref para manejar el video
+
+  const handleError = (error) => {
+    console.error('Error al cargar el video:', error);
+  };
+
   return (
     <View style={styles.container}>
       <Text style={styles.title}>{channelName}</Text>
       <Video
-        source={{ uri: channelUrl }}
+        ref={videoRef}
+        source={{ uri: channelUrl }} // Asegúrate de que este sea un enlace HTTP válido
         style={styles.video}
         useNativeControls
         resizeMode="contain"
         shouldPlay
+        onError={handleError} // Maneja errores al cargar el video
       />
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  container: { flex: 1, justifyContent: 'center', alignItems: 'center' },
-  title: { fontSize: 24, marginBottom: 20 },
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  title: {
+    fontSize: 24,
+    marginBottom: 20,
+  },
   video: {
     width: '100%',
     height: Dimensions.get('window').height * 0.4,
